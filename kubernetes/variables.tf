59d2db41c959ce23de408b5e114244c798b6ff6f
ayuSH8033 <99918376+ayuSH8033@users.noreply.github.com>
2023-05-12 20:33:42 +0530

    Updated variables.tf

--- kubernetes/variables.tf
+++ kubernetes/variables.tf
@@ -5,13 +5,6 @@ variable "aws_region" {
   default     = "ap-south-1"
 }
 
-variable "aws_access_key" {
-default = “AKIA5DG4WPHPBDHW5M6M”
-}
-
-variable "aws_secret_key" {
-default = “y47UYrzETjfgguiMd/StRxQzAr/FnLy3t6fd1xfa”
- }
 
 variable "bucket_name" {
   description = "S3 bucket name"
