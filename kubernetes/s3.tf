e45a164060253456b7131210ed4ccfd48a50c2a0
ayuSH8033 <99918376+ayuSH8033@users.noreply.github.com>
2023-05-12 19:02:12 +0530

    Updated s3.tf

--- kubernetes/s3.tf
+++ kubernetes/s3.tf
@@ -1,14 +1,40 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+
+  required_version = ">= 0.15"
+}
+
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.region}"
+  profile = "default"
+  region  = var.aws_region
 }
 
-module "s3" {
-    source = "/exp/new"
-    bucket_name = "earth-bucket"       
+
+
+########################
+# Bucket creation
+########################
+resource "aws_s3_bucket" "earthlybucket" {
+  bucket = var.bucket_name
 }
 
-resource "aws_s3_bucket" "temps3" {
-    acl = "${var.acl_value}"   
+
+
+########################
+# Disabling bucket
+# public access
+########################
+resource "aws_s3_bucket_public_access_block" "earthlybucket_access" {
+  bucket = aws_s3_bucket.earthlybucket.id
+
+  # Block public access
+  block_public_acls   = true
+  block_public_policy = true
+  ignore_public_acls = true
+  restrict_public_buckets = true
 }
