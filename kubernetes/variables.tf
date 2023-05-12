856b8406bd4dbbd2b0f435bb2ccc913e4d03ff4b
ayuSH8033 <99918376+ayuSH8033@users.noreply.github.com>
2023-05-12 19:12:37 +0530

    Updated variables.tf

--- kubernetes/variables.tf
+++ kubernetes/variables.tf
@@ -1,40 +1,12 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0.0"
-    }
-  }
 
-  required_version = ">= 0.15"
+variable "aws_region" {
+  description = "AWS region"
+  type        = string
+  default     = "ap-south-1"
 }
 
-provider "aws" {
-  profile = "default"
-  region  = var.aws_region
-}
-
-
-
-########################
-# Bucket creation
-########################
-resource "aws_s3_bucket" "earthlybucket" {
-  bucket = var.bucket_name
-}
-
-
-
-########################
-# Disabling bucket
-# public access
-########################
-resource "aws_s3_bucket_public_access_block" "earthlybucket_access" {
-  bucket = aws_s3_bucket.earthlybucket.id
-
-  # Block public access
-  block_public_acls   = true
-  block_public_policy = true
-  ignore_public_acls = true
-  restrict_public_buckets = true
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+  default     = "earthr57y56786"
 }
